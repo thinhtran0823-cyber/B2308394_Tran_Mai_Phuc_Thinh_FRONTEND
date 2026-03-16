9194e6f7578c066d55ae4e64c40de6b28aa7523c
TranMaiPhucThinh <thinhtran0823@gmail.com>
2026-03-16 16:29:14 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,17 @@ const routes = [
     name: "notfound",
     component: () => import("@/views/NotFound.vue"),
   },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true 
+  },
+  {
+    path: "/contacts/",
+    name: "contact.add",
+    component: () => import("@/views/ContactAdd.vue"),
+  },
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
